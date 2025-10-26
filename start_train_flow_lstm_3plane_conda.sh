35c3f8fd1ad3cbe5cbf5ed1ee84fdcd0272bf7d7
CB_sjtu <cb_sjtu@sjtu.edu.cn>
2025-10-26 09:42:21 +0800

    hyperparameter

--- start_train_flow_lstm_3plane_conda.sh
+++ start_train_flow_lstm_3plane_conda.sh
@@ -6,7 +6,7 @@ echo "Starting training for flow_lstm_3plane model..."
 
 # 1) 训练
 echo "Starting training..."
-python src/train.py --config-name=train_flow_lstm_3plane trainer.max_steps=50000 trainer.val_check_interval=300 trainer.limit_val_batches=5
+python src/train.py --config-name=train_flow_lstm_3plane trainer.max_steps=20000 trainer.val_check_interval=300 trainer.limit_val_batches=5
 echo "Training completed!"
 
 # 2) 等待 wandb 同步
